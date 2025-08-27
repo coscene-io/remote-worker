7aa3380a06f6b54efb13105fe26d624668c9c4b8
龙广博 <45959574+LEGO5543@users.noreply.github.com>
2025-08-27 17:46:39 +0800

    Add files via upload

---
+++ build/roarm_moveit/roarm_moveit__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pbdq/roarm_ws/build/roarm_moveit/roarm_moveit__py
+# Build directory: /home/pbdq/roarm_ws/build/roarm_moveit/roarm_moveit__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
