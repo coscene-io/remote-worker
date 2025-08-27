997d34d0a0ed7b75988380a767bf9e1e4ed5e319
龙广博 <45959574+LEGO5543@users.noreply.github.com>
2025-08-27 17:37:05 +0800

    Add files via upload

---
+++ build/roarm_bringup/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,7 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/pbdq/roarm_ws/install/ik_solver/share/ik_solver/package.sh"
+. "/home/pbdq/roarm_ws/install/roarm_description/share/roarm_description/package.sh"
+. "/home/pbdq/roarm_ws/install/roarm_driver/share/roarm_driver/package.sh"
+. "/home/pbdq/roarm_ws/install/roarm_moveit_ikfast_plugins/share/roarm_moveit_ikfast_plugins/package.sh"
+. "/home/pbdq/roarm_ws/install/sirius_reader/share/sirius_reader/package.sh"
+. "/home/pbdq/roarm_ws/install/roarm_moveit/share/roarm_moveit/package.sh"
